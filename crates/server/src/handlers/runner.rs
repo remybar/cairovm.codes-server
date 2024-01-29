7325798f81da28ac3f23340e18c98412d7f189a8
Roman <30427286+barabanovro@users.noreply.github.com>
2024-01-29 20:39:28 +0400

    feat: Return execution output at the 'run' endpoint (#2)

--- crates/server/src/handlers/runner.rs
+++ crates/server/src/handlers/runner.rs
@@ -1,19 +1,8 @@
 use axum::{http::StatusCode, Json};
-use cairo_lang_compiler::{compile_cairo_project_at_path, CompilerConfig};
-use cairo_lang_sierra_ap_change::calc_ap_changes;
-use cairo_lang_sierra_gas::gas_info::GasInfo;
-use cairo_lang_sierra_to_casm::metadata::calc_metadata;
-use cairo_lang_sierra_to_casm::metadata::Metadata;
-use cairo_lang_sierra_to_casm::metadata::MetadataComputationConfig;
-use cairo_lang_sierra_to_casm::metadata::MetadataError;
-use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
+use cairo1_run::{run_program_at_path, RunResult, CAIRO_LANG_COMPILER_VERSION};
 use rand::distributions::{Distribution, Uniform};
 use serde::{Deserialize, Serialize};
-use std::env;
-use std::fs;
-use std::path::PathBuf;
-
-const CAIRO_LANG_COMPILER_VERSION: &'static str = "2.5.0";
+use std::{env, fs, path::PathBuf};
 
 fn write_to_temp_file(content: &str) -> PathBuf {
     let current_dir = env::current_dir().expect("Failed to get current directory");
@@ -37,6 +26,7 @@ pub struct RunnerResult {
     sierra_program_code: String,
     casm_program_code: String,
     cairo_lang_compiler_version: String,
+    serialized_output: Option<String>,
 }
 
 pub async fn runner_handler(
@@ -44,44 +34,15 @@ pub async fn runner_handler(
 ) -> Result<Json<RunnerResult>, StatusCode> {
     let file_path = write_to_temp_file(&payload.cairo_program_code);
 
-    let compiler_config = CompilerConfig {
-        replace_ids: true,
-        ..CompilerConfig::default()
-    };
-    let sierra_program = match compile_cairo_project_at_path(&file_path, compiler_config) {
-        Ok(program) => program,
-        Err(err) => {
-            // Delete the temporary file if an error occurs
+    let RunResult {
+        sierra_program,
+        casm_program,
+        serialized_output,
+    } = match run_program_at_path(&file_path) {
+        Ok(result) => result,
+        Err(error) => {
+            dbg!(error);
             fs::remove_file(&file_path).expect("Failed to delete temporary file");
-            println!(
-                "Failed to compile cairo program; error: {}",
-                err.to_string()
-            );
-            return Err(StatusCode::EXPECTATION_FAILED);
-        }
-    };
-
-    let metadata_config = Some(Default::default());
-
-    let gas_usage_check = metadata_config.is_some();
-    let metadata = match create_metadata(&sierra_program, metadata_config) {
-        Ok(metadata) => metadata,
-        Err(_) => {
-            fs::remove_file(&file_path).expect("Failed to delete temporary file");
-            println!("Failed to compute metadata");
-            return Err(StatusCode::EXPECTATION_FAILED);
-        }
-    };
-
-    let casm_program = match cairo_lang_sierra_to_casm::compiler::compile(
-        &sierra_program,
-        &metadata,
-        gas_usage_check,
-    ) {
-        Ok(casm_program) => casm_program,
-        Err(_) => {
-            fs::remove_file(&file_path).expect("Failed to delete temporary file");
-            println!("Failed to compile sierra program");
             return Err(StatusCode::EXPECTATION_FAILED);
         }
     };
@@ -93,27 +54,6 @@ pub async fn runner_handler(
         sierra_program_code: format!("{sierra_program}"),
         casm_program_code: format!("{casm_program}"),
         cairo_lang_compiler_version: CAIRO_LANG_COMPILER_VERSION.to_string(),
+        serialized_output,
     }))
-}
-
-/// Creates the metadata required for a Sierra program lowering to casm.
-fn create_metadata(
-    sierra_program: &cairo_lang_sierra::program::Program,
-    metadata_config: Option<MetadataComputationConfig>,
-) -> Result<Metadata, VirtualMachineError> {
-    if let Some(metadata_config) = metadata_config {
-        calc_metadata(sierra_program, metadata_config).map_err(|err| match err {
-            MetadataError::ApChangeError(_) => VirtualMachineError::Unexpected,
-            MetadataError::CostError(_) => VirtualMachineError::Unexpected,
-        })
-    } else {
-        Ok(Metadata {
-            ap_change_info: calc_ap_changes(sierra_program, |_, _| 0)
-                .map_err(|_| VirtualMachineError::Unexpected)?,
-            gas_info: GasInfo {
-                variable_values: Default::default(),
-                function_costs: Default::default(),
-            },
-        })
-    }
 }
